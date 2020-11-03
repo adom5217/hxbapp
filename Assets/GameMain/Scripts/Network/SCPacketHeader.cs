72fa6863b5648034efc9ff0082e2a26617286e83
adom <245344450@qq.com>
2020-11-03 12:39:16 +0800

    框架梳理

---
+++ Assets/GameMain/Scripts/Network/SCPacketHeader.cs
@@ -0,0 +1,20 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2020 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+namespace StarForce
+{
+    public sealed class SCPacketHeader : PacketHeaderBase
+    {
+        public override PacketType PacketType
+        {
+            get
+            {
+                return PacketType.ServerToClient;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+﻿## HOMEPAGE
+
+---
+
+##  项目简介
+
+使用 Game Framework 游戏框架制作的游戏项目，模仿荒芜乱斗单机版。
+
+---
+
+![Game Framework](https://gameframework.cn)
+
+---
+
+## Game Framework 简介
+
+Game Framework 是一个基于 Unity 引擎的游戏框架，主要对游戏开发过程中常用模块进行了封装，很大程度地规范开发过程、加快开发速度并保证产品质量。
+
+在最新的 Game Framework 版本中，包含以下 19 个内置模块，后续我们还将开发更多的扩展模块供开发者使用。
+
